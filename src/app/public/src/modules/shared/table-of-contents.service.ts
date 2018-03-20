e0e488ae582853010574b51ea9d7ad2dac16421c
bdjones2410 <brandon.jones@blackbaud.com>
2018-03-20 15:53:46 -0400

    added service

---
+++ src/app/public/src/modules/shared/table-of-contents.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import 'rxjs/Rx';
+import { Subject, Subscription } from 'rxjs/Rx';
+
+@Injectable()
+export class StacheTOCService {
+  private eventBus$ = new Subject();
+
+  subscribe(next: any, error?: any, complete?: any) {
+    return this.eventBus$.subscribe(next, error, complete);
+  }
+
+  next(event: any) {
+    this.eventBus$.next(event);
+  }
+}
