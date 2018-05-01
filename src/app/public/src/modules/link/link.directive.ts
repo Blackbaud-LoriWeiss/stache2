267b33dce1ea59d20dfeb36c68b4cc7acb595b11
bdjones2410 <brandon.jones@blackbaud.com>
2018-05-01 12:59:10 -0400

    demo stuff

--- src/app/public/src/modules/link/link.directive.ts
+++ src/app/public/src/modules/link/link.directive.ts
@@ -1,27 +1,60 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding, Input, Renderer2, OnChanges, AfterViewInit } from '@angular/core';
+
+import { LocationStrategy } from '@angular/common';
 
 import { StacheNavService } from '../nav';
+
+import { StacheRouteService } from '../shared';
 
 @Directive({
   selector: '[stacheRouterLink]'
 })
-export class StacheRouterLinkDirective {
-  @Input()
-  public stacheRouterLink: string;
+export class StacheRouterLinkDirective implements OnChanges, AfterViewInit {
+
+  public _stacheRouterLink: string = '';
+
+  @Input('stacheRouterLink')
+  set stacheRouterLink(routerLink: string) {
+    this._stacheRouterLink = routerLink;
+  }
+
+  get stacheRouterLink() {
+    if (this._stacheRouterLink === '.') {
+      return this.routerService.getActiveUrl();
+    }
+    return this._stacheRouterLink;
+  }
 
   @Input()
   public fragment: string;
 
+  // the url displayed on the anchor element.
+  @HostBinding()
+  href: string;
+
   constructor(
     private navService: StacheNavService,
+    private routerService: StacheRouteService,
     private el: ElementRef,
+    private locationStrategy: LocationStrategy,
     private renderer: Renderer2
   ) {
     this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
   }
 
+  ngOnChanges(changes: {}): any { this.updateTargetUrlAndHref(); }
+
+  ngAfterViewInit() {
+    this.updateTargetUrlAndHref();
+  }
+
+  private updateTargetUrlAndHref(): void {
+    this.href = this.locationStrategy.prepareExternalUrl(`${this.stacheRouterLink}#${this.fragment}`);
+  }
+
   @HostListener('click', ['$event'])
   public navigate(event: MouseEvent): void {
+    console.log('clicked', this.stacheRouterLink);
     event.preventDefault();
     this.navService.navigate({
       path: this.stacheRouterLink,
